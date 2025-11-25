ded85c07addd0ff319513cf25a7c38af10554d9e
carmecha <carmecha@gmail.com>
2025-11-25 17:15:03 -0300

    Add src folder with all files

---
+++ src/main/java/com/carmendiaz/e_commerce/carmendiaz/CarmendiazApplication.java
@@ -0,0 +1,13 @@
+package com.carmendiaz.e_commerce.carmendiaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarmendiazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarmendiazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carmendiaz/e_commerce/carmendiaz/CarmendiazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carmendiaz.e_commerce.carmendiaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarmendiazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
